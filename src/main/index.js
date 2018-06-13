c03fdde0b62c7cb8fdfb98aa4c2a6b51d504ef80
Alan Zhang <alandevel@outlook.com>
2018-06-13 16:35:41 -0400

    fix maxBuffer exceeded error

--- src/main/index.js
+++ src/main/index.js
@@ -138,7 +138,7 @@ app.on('ready', () => {
 // aria2
 function startAria2 () {
   const AppData = require('./appdata').default
-  const exec = require('child_process').exec
+  const spawn = require('child_process').spawn
   const join = require('path').join
   const platform = require('os').platform()
   const homedir = require('os').homedir()
@@ -160,16 +160,13 @@ function startAria2 () {
   }, AppData.readData() || {})
   if (!options.hasOwnProperty('dir')) options['dir'] = join(homedir, 'Downloads')
 
-  let command = '"' + aria2c + '" --conf-path="' + conf + '"'
-  for (let key in options) command += ' --' + key + '="' + options[key] + '"'
-  return exec(command, (error, stdout, stderr) => {
-    if (error) {
-      console.error(error.message)
-      const message = 'conflicts with an existing aria2 instance. Please stop the instance and reopen the app.'
-      dialog.showErrorBox('Warning', message)
-      app.quit()
-    }
-    if (stderr) console.warn(stderr)
-    if (stdout) console.log(stdout)
+  let command = '"' + aria2c + '"'
+  let args = ['--conf-path="' + conf + '"']
+  for (let key in options) args.push('--' + key + '="' + options[key] + '"')
+  let subprocess = spawn(command, args, { shell: true })
+  subprocess.on('exit', (code, signal) => {
+    if (code === 1) dialog.showErrorBox('Warning', 'conflicts with an existing aria2 instance. Please stop the instance and reopen the app.')
+    app.quit()
   })
+  return subprocess
 }
